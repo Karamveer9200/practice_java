7d6715246cc0bfa2c8d8308cb5963d1d982d5850
jhondoe9200 <jhondoe9200@gmail.com>
2023-09-11 04:42:20 +0530

    added ch10_8

---
+++ src/beginning/oops/basics/ch10_8_exercise_online_library.java
@@ -0,0 +1,29 @@
+package beginning.oops.basics;
+
+// You have to implement a library using Java Class "Library"
+// Methods: addBook, issueBook, returnBook, showAvailableBooks
+// Properties: Array to store the available books,
+// Array to store the issued books
+
+class Library{
+    static void addBook(){
+
+    }
+    static void issueBook(){
+
+    }
+    static void returnBook(){
+
+    }
+    static void showAvailableBooks(){
+
+    }
+}
+
+public class ch10_8_exercise_online_library {
+    public static void main(String[] args) {
+        Library school = new Library();
+        
+
+    }
+}
