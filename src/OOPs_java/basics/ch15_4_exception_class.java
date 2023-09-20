2c21f30276af3ac52c2c8ac215fd7b0a9a7afb1a
jhondoe9200 <jhondoe9200@gmail.com>
2023-09-20 21:16:27 +0530

    1 new entry ch15

---
+++ src/OOPs_java/basics/ch15_4_exception_class.java
@@ -0,0 +1,19 @@
+package OOPs_java.basics;
+
+// Exception class in Java
+// We can write and 'throw' our custom exceptions using Exception class in Java.
+// For that, we have to 'extend' Exception class and override its methods
+
+//  public class MyException extends Exception{
+//  overridden methods
+//  }
+
+class MyException extends Exception{
+
+}
+
+public class ch15_4_exception_class {
+    public static void main(String[] args) {
+
+    }
+}
